dfd42cf1306cbaa8c2b45ebcb99609baf8194dfa
Katsume Yuichiro <yuichiro@katsu.me>
2013-08-15 13:12:16 +0900

    modify arduino

--- arduino/RFIDSender/RFIDSender.ino
+++ arduino/RFIDSender/RFIDSender.ino
@@ -14,29 +14,36 @@ void setup() {
 	}
 
 	nfc.setPassiveActivationRetries(0xFF);
-
 	nfc.SAMConfig();
+
+	while(Serial.available()<=0){
+		Serial.println(0UL, HEX);
+		delay(200);
+	}
 }
 
 void loop() {
 
-	boolean			success;
-	uint8_t			uid[]= { 0, 0, 0, 0, 0, 0, 0 };
-	uint8_t			uidLength;
-	unsigned long	uid2= 0;
-	
-	success= nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-	
-	if (success && uidLength==4) {
+	if(Serial.available()>0){
 
-		for (uint8_t i=0; i<uidLength; i++) {
-			unsigned long tmp= uid[i];
-			tmp= tmp<<(uidLength-1-i)*8;
-			uid2= uid2 | tmp;
+		boolean			success;
+		uint8_t			uid[]= { 0, 0, 0, 0, 0, 0, 0 };
+		uint8_t			uidLength;
+		unsigned long	uid2= 0;
+
+		Serial.read();
+
+		success= nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+	
+		if (success && uidLength==4) {
+
+			for (uint8_t i=0; i<uidLength; i++) {
+				unsigned long tmp= uid[i];
+				tmp= tmp<<(uidLength-1-i)*8;
+				uid2= uid2 | tmp;
+			}
+
+			Serial.println(uid2, HEX);
 		}
-
-		Serial.println(uid2, HEX);
-		
-		delay(66);
 	}
 }
